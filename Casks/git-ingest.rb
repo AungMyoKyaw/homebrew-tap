9745dc61a02999ba847c45c423eec2cfe0e1c6dd
Aung Myo Kyaw <aungmyokyaw@users.noreply.github.com>
2026-07-11 16:17:22 +0700

    chore(cask): update git-ingest to 0.2.7

--- Casks/git-ingest.rb
+++ Casks/git-ingest.rb
@@ -1,9 +1,9 @@
 cask "git-ingest" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.6"
-  sha256 arm:   "ed2e2bdafd39fe09290c875b3020fbf23a12ea445e2369e9180c7d0bdf5020c2",
-         intel: "af1e71a77acc23fe5934be887f98b559c3a9be43ed3dde148ec0581cf61fce3c"
+  version "0.2.7"
+  sha256 arm:   "856b998fc24e132768c6e9d7c866a5fa97ee6a767899a29454b367040107c01a",
+         intel: "1469e09bb6e3472959542c168c5bdfd0c213067128ac1c9082f15d204d773e0a"
 
   url "https://github.com/AungMyoKyaw/git-ingest-desktop/releases/download/v#{version}/Git-Ingest-#{version}-#{arch}.dmg"
   name "Git-Ingest"

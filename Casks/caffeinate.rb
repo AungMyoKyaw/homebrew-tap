a724d6eb4e48410ce99516d52b0c6ac6a3e906fd
Aung Myo Kyaw <aungmyokyaw@users.noreply.github.com>
2026-08-01 19:36:37 +0700

    fix: use correct sha256 for released Caffeinate DMG

--- Casks/caffeinate.rb
+++ Casks/caffeinate.rb
@@ -1,6 +1,6 @@
 cask "caffeinate" do
   version "1.0.0"
-  sha256 "497fb07b63cce9c09e6979f53a901af5bc95aaf256543d2deccf8f868bb75ef1"
+  sha256 "ed526390d24cb8a2b969a00f0d5dfbbf51e8661d9774d1b737a882639c2c23a8"
 
   url "https://github.com/AungMyoKyaw/caffeinate-app/releases/download/v#{version}/Caffeinate_#{version}_universal.dmg"
   name "Caffeinate"

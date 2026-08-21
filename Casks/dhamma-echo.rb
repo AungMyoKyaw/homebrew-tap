bba2b2f3ac6079024478dacf5521f204d7ee33f1
Aung Myo Kyaw <aungmyokyaw@users.noreply.github.com>
2026-08-21 10:05:08 +0700

    chore(cask): update Dhamma Echo to 0.5.7

--- Casks/dhamma-echo.rb
+++ Casks/dhamma-echo.rb
@@ -1,9 +1,9 @@
 cask "dhamma-echo" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.5.6"
-  sha256 arm:   "aa58769bea12ed267eb95d96082f915ef5ae4e9f03a14b78235b05e1bc8528b9",
-         intel: "6246da2409db57b06023372ecd421e3f3903b91b06bf8ad320afa80529134dd1"
+  version "0.5.7"
+  sha256 arm:   "cc20e0acb8442408b821f47f2d9a91ec637b3619fcd0135d58164556b0d56dbc",
+         intel: "c26a91bf82776dcd360988ffa57dfcd86aafeb876849591d9e9c34e9d8b81222"
 
   url "https://github.com/AungMyoKyaw/dhamma-echo/releases/download/v#{version}/Dhamma.Echo_#{version}_#{arch}.dmg"
   name "Dhamma Echo"
